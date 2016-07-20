0e424d008b6ce0206211d3c0730ae7d52ad48a2d
QI WANG <Daisy@QIs-MacBook-Pro.local>
2016-07-20 04:27:14 +0100

    add search

---
+++ index.android.js
@@ -0,0 +1,49 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+
+var OutlineNav = require('./OutlineNav');
+var FrontNav = require('./FrontNav');
+var SearchNav = require('./SearchNav');
+
+var ScrollableTabView = require('react-native-scrollable-tab-view');
+
+class Good extends Component {
+  render(){
+    return (
+      <ScrollableTabView tabBarPosition='bottom'
+            initialPage={0}>
+        <FrontNav tabLabel="Featured" />
+        <OutlineNav tabLabel="Popular" />
+        <SearchNav tabLabel="Search" />
+
+      </ScrollableTabView>
+
+    );
+  }
+}
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  toolbar: {
+    backgroundColor: '#a9a9a9',
+    height: 56,
+  },
+});
+
+
+AppRegistry.registerComponent('Good', () => Good);
